cde7f2e0cb8e4b79524864fb091e67d882d1cfa3
Conor <K2045414@Kingston.ac.uk>
2023-02-24 12:50:13 +0000

    Commit Test

--- FirstIteration/Form1.cs
+++ FirstIteration/Form1.cs
@@ -16,5 +16,10 @@ namespace FirstIteration
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Changed";
+        }
     }
 }
