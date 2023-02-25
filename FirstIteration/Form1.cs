97aba7642c2b839db815b4bbae51bdd2530b269d
Conor <K2045414@Kingston.ac.uk>
2023-02-25 00:26:02 +0000

    CKDEPI Implementation - Needs much more testing and age calculator is busted

--- FirstIteration/Form1.cs
+++ FirstIteration/Form1.cs
@@ -19,7 +19,7 @@ namespace FirstIteration
 
         private void BTN_Calculate_Click(object sender, EventArgs e)
         {
-            int Creatinine = Int32.Parse(RTB_Creatinine.Text);
+            double Creatinine = double.Parse(RTB_Creatinine.Text);
             DateTime DOB = DTP_DOB.Value;
             DateTime Current = DateTime.Now;
             TimeSpan Diff = Current - DOB;
@@ -27,26 +27,54 @@ namespace FirstIteration
             int Age = (int)((days - 365) / 365);
             string Gender = CBX_Gender.Text;
             string Ethnicity = CBX_Ethnicity.Text;
-            double GFR = MDRD(Creatinine, Age, Gender, Ethnicity);
-            string Test = "MDRD: " + GFR;
+            double GFR = CKDEPI(Creatinine, Age, Gender, Ethnicity);
+            string Test = "CKDEPI: " + GFR;
             RTB_eGFR.Text = Test;
         }
-        public double MDRD(int Creatinine, int Age, string Gender, string Ethnicity)
+        public double MDRD(double Creatinine, int Age, string Gender, string Ethnicity)
         {
-            double k = 1;
+            double g = 1;
             if (Gender == "Female")
             {
-                k = 0.742;
+                g = 0.742;
             }
             double a = -1.154;
             double b = -0.203;
-            double c = 1;
+            double e = 1;
             if (Ethnicity == "Black")
             {
-                c = 1.210;
+                e = 1.210;
             }
-            double GFR = 186 * Math.Pow((Creatinine / 88.4), a) * Math.Pow(Age, b) * k * c;
+            double GFR = 186 * Math.Pow((Creatinine / 88.4), a) * Math.Pow(Age, b) * g * e;
             return GFR;
         }
+        public double CKDEPI(double Creatinine, int Age, string Gender, string Ethnicity)
+        {
+            double k;
+            double a;
+            double g = 1;
+            double e = 1;
+            if (Gender == "Female")
+            {
+                k = 0.7;
+                a = -0.329;
+                g = 1.018;
+            }
+            else
+            {
+                k = 0.9;
+                a = -0.411;                
+            }
+
+            if (Ethnicity == "Black")
+            {
+                e = 1.159;
+            }
+
+            double GFR = 141 * Math.Pow(Math.Min(Creatinine / k, 1), a) * Math.Pow(Math.Max(Creatinine / k, 1), -1.209) * Math.Pow(0.993, Age) * g * e;
+            return GFR;
+        }
+
+
     }
 }
